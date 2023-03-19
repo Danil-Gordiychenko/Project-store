ebc0526082d2945dffe194f6ce618b6c85bc72ee
Danil-Gordiychenko <d.p.gordiychenko@student.khai.edu>
2023-03-19 18:15:03 -0400

    my project

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+// let userName = 'Art';
+// let userAge = 31;
+// console.log(userName + ' is ' + userAge + ' years old');
+
+// userHeight = 170; 
+// console.log(userHeight); 
+// console.log(`2 + 2 = ${2+2}`);
+
+// let a = 10;
+// let b = a;
+// console.log(`посчитаем a + b = ${a + b}`);
+
+// const COLOR_RED = "red"; 
+// const COLOR_GREEN = "green"; 
+// const COLOR_BLUE = "blue"; 
+// const COLOR_ORANGE = "orange";
+
+// let COLOR = COLOR_GREEN; 
+// console.log(COLOR); 
+// color = COLOR_GREEN;
+// console.log(color);
